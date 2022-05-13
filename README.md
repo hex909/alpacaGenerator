d752c03d8bc6dfce029938795d402ab853aee203
hex909 <tst.flopp@gmail.com>
2022-05-14 00:38:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# DevProjects - Alpaca image generator website
+
+This is an open source project from [DevProjects](http://www.codementor.io/projects). Feedback and questions are welcome!
+Find the project requirements here: [Alpaca image generator website](https://www.codementor.io/projects/web/alpaca-image-generator-website-ce2oc0eus8)
+
+## Tech/framework used
+
+Built with [React](https://reactjs.org/)
+
+## Screenshots and demo
+
+![](./screenshot.png)
+
+## Installation
+
+1. Run `git clone https://github.com/hex909/alpacaGenerator.git && cd alpacaGenerator`
+
+2. Run `npm i`.
+
+3. Run `npm run dev`.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/imgData.js
@@ -0,0 +1,66 @@
+const data = {
+  hair: {
+    path: "alpaca/hair",
+    photos: ["default", "bang", "curls", "elegant", "fancy", "quiff", "short"],
+  },
+  ears: {
+    path: "alpaca/ears",
+    photos: ["default", "tilt-backward", "tilt-forward"],
+  },
+  eyes: {
+    path: "alpaca/eyes",
+    photos: ["default", "angry", "naughty", "panda", "smart", "star"],
+  },
+  mouth: {
+    path: "alpaca/mouth",
+    photos: ["default", "astonished", "eating", "laugh", "tongue"],
+  },
+  neck: {
+    path: "alpaca/neck",
+    photos: ["default", "bend-backward", "bend-forward", "thick"],
+  },
+  nose: {
+    path: "alpaca/nose",
+    photos: [],
+  },
+
+  leg: {
+    path: "alpaca/leg",
+    photos: [
+      "default",
+      "bubble-tea",
+      "cookie",
+      "game-console",
+      "tilt-backward",
+      "tilt-forward",
+    ],
+  },
+  accessories: {
+    path: "alpaca/accessories",
+    photos: ["earings", "flower", "glasses", "headphone"],
+  },
+  backgrounds: {
+    path: "alpaca/backgrounds",
+    photos: [
+      "blue50",
+      "blue60",
+      "blue70",
+      "darkblue30",
+      "darkblue50",
+      "darkblue70",
+      "green50",
+      "green60",
+      "green70",
+      "grey40",
+      "grey70",
+      "grey80",
+      "red50",
+      "red60",
+      "red70",
+      "yellow50",
+      "yellow60",
+      "yellow70",
+    ],
+  },
+};
+export default data;
